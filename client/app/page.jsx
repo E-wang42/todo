68b7fcd5de29f6de10426d47661a5a9c3a93ae5e
eric <ericwang42@outlook.com>
2023-04-14 12:50:55 -0700

    update

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,12 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import sun from "../public/icon-sun.svg";
 import TodoList from "../components/TodoList";
 
 export default function Home() {
   const [description, setDescription] = useState("");
-  console.log(description);
+  const inputRef = useRef(); //input focus on load
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -19,6 +23,8 @@ export default function Home() {
       console.log(response);
     } catch (err) {
       console.error(err);
+    } finally {
+      setDescription("");
     }
   }
 
@@ -26,7 +32,7 @@ export default function Home() {
     <div>
       <div className="container relative">
         <div className="absolute -z-10 h-[13rem] w-full bg-topBg bg-cover bg-center bg-no-repeat"></div>
-        <div className="mx-auto flex h-full w-5/6 flex-col justify-evenly py-12 ring-2">
+        <div className="mx-auto flex h-full w-5/6 flex-col justify-evenly py-12">
           <div className="flex flex-row items-center justify-between pb-8">
             <h1 className="text-xl font-bold tracking-[0.6rem] text-white">
               TODO
@@ -37,6 +43,7 @@ export default function Home() {
           </div>
           <form onSubmit={handleSubmit} className="w-full">
             <input
+              required
               className="w-full p-2 focus:outline-none"
               type="text"
               placeholder="Create new todo..."
@@ -44,6 +51,7 @@ export default function Home() {
               onChange={(e) => setDescription(e.target.value)}
               value={description}
               name="text"
+              ref={inputRef}
             />
           </form>
           <div className="relative mt-8 w-full ring-2">
--- client/components/TodoList.jsx
+++ client/components/TodoList.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+//write async api call.  try suspense
+
 function TodoList(props) {
   return (
     <li className="flex w-full flex-row items-center justify-start bg-white p-2">
